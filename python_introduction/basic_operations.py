9bc56eba77572e9b3a7d216e72abdbaac8e40042
clivian <clivianvoster@gmail.com>
2025-09-14 22:41:09 +0300

    Add print statement

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,8 @@
 number1 = 10
 number2 = 5
-print Addition = number1 + number2
-print Subtraction = number1 - number2
-print Multiplication = number1 * number2
-
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+print (Addition of {number1} and {number2} is: {15}.)
+print (Subtraction of {number1} and {number2} is: {5}.)
+print (Multiplication of {number1} and {number2} is: {50}.)
