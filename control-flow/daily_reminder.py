dae53a873d9e178b16575a60360f91b6a5fa4f1c
clivian <clivianvoster@gmail.com>
2025-09-18 14:21:27 +0300

    Save

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -14,4 +14,4 @@ if time_bound == "yes":
     reminder += " that requires immediate attention today!"
 else:
     reminder += " Consider completing it when you have free time."
-print(reminder)
+print(f"{reminder}")
