668313743d03c5d0c0463e0e7fda034c08dec603
clivian <clivianvoster@gmail.com>
2025-09-22 15:53:59 +0300

    add prompt wordings

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -20,7 +20,7 @@ def main():
     display_current_datetime()
 
     try:
-        days = int(input("Enter the number of days to add: "))
+        days = int(input("Enter the number of days to add to the current date: "))
         calculate_future_date(days)
     except ValueError:
         print("Please enter a valid integer for days.")
